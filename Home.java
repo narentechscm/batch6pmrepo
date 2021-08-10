bfac13c518b0321d563327e935d529abd0b75f42
root <root@ip-172-31-46-64.ap-southeast-1.compute.internal>
2021-08-10 13:45:40 +0000

    First project chaanges

---
+++ Home.java
@@ -0,0 +1,6 @@
+class Home{
+	public staic void main(String []args){
+		System.out.println("Hello from Home java file");
+		}
+	}
+
---
+++ Login.java
@@ -0,0 +1,6 @@
+class Login{
+	public staic void main(String []args){
+		System.out.println("Hello from Login java file");
+		}
+	}
+
